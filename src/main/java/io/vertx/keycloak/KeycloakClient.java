ae3861ae28dbda4c02b94bb952df65d66096f28f
oknozor <paul.delafosse@protonmail.com>
2018-08-31 13:56:29 +0200

    first commit

---
+++ src/main/java/io/vertx/keycloak/KeycloakClient.java
@@ -0,0 +1,58 @@
+package io.vertx.keycloak;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.MultiMap;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.client.WebClient;
+
+
+class KeycloakClient extends AbstractVerticle {
+
+  private WebClient webClient;
+  private JsonObject config;
+
+  protected KeycloakClient(WebClient webClient, JsonObject config) {
+    this.webClient = webClient;
+    this.config = config;
+  }
+
+  public void requestBearerToken(RoutingContext routingContext) {
+
+    final JsonObject keycloakConfig = config.getJsonObject("keycloak");
+    final String keycloakHost = keycloakConfig.getString("host");
+    final String keycloakClientUri = keycloakConfig.getString("client.uri");
+    final String keycloakBasicAuth = keycloakConfig.getString("client.basic");
+
+    final String synapseHost = config.getString("synapse.host");
+
+    JsonObject userInfo = routingContext.getBodyAsJson();
+    String username = userInfo.getJsonObject("user").getString("id");
+
+    final String keycloakPassword = userInfo.getJsonObject("user").getString("password");
+    username = username.replace(":" + synapseHost, "");
+    username = username.substring(1);
+    System.out.println(username);
+    final String keycloakUsername = username;
+
+    System.out.println(userInfo);
+    MultiMap form = MultiMap.caseInsensitiveMultiMap();
+    form.add("username", keycloakUsername);
+    form.add("password", keycloakPassword);
+    form.add("grant_type", "password");
+
+    webClient.post(443, keycloakHost, keycloakClientUri)
+      .putHeader("Authorization", keycloakBasicAuth)
+      .putHeader("content-type", "application/x-www-form-urlencoded")
+      .ssl(true)
+      .sendForm(form, ar -> {
+      if (ar.succeeded()) {
+        System.out.println(ar.result().statusCode());
+        System.out.println(ar.result().bodyAsJsonObject());
+        ar.succeeded();
+      } else {
+        System.out.println("nope");
+      }
+    });
+  }
+}
---
+++ src/main/java/io/vertx/keycloak/MainVerticle.java
@@ -0,0 +1,43 @@
+package io.vertx.keycloak;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.client.WebClient;
+import io.vertx.ext.web.client.WebClientOptions;
+import io.vertx.ext.web.handler.BodyHandler;
+
+public class MainVerticle extends AbstractVerticle {
+
+  @Override
+  public void start(Future<Void> startFuture) {
+
+    JsonObject clientConfig = config();
+    WebClient webClient = WebClient.create(vertx, new WebClientOptions()
+      .setSsl(true)
+      .setUserAgent("synapse-adapter"));
+    KeycloakClient keycloakClient = new KeycloakClient(webClient, clientConfig);
+
+    HttpServer server = vertx.createHttpServer();
+    Router router = Router.router(vertx);
+
+    router.route().handler(BodyHandler.create());
+    router.post("/_matrix-internal/identity/v1/check_credentials")
+      .handler(keycloakClient::requestBearerToken);
+
+    Integer portNumber = config().getInteger("http.port");
+
+    server.requestHandler(router::accept)
+      .listen(portNumber, http -> {
+        if (http.succeeded()) {
+          startFuture.complete();
+          System.out.println("HTTP server started on http://localhost:8080");
+        } else {
+          startFuture.fail(http.cause());
+        }
+      });
+  }
+
+}
