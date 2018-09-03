1c00d2c915590e9807a0614fcb389ec283e06296
oknozor <paul.delafosse@protonmail.com>
2018-09-03 17:19:32 +0200

    wip(handlers) extract request generation

--- src/main/java/hoohoot/synapse/adapter/http/clients/MxisdHandler.java
+++ src/main/java/hoohoot/synapse/adapter/http/clients/MxisdHandler.java
@@ -5,11 +5,16 @@ import hoohoot.synapse.adapter.http.HttpJsonErrors;
 import hoohoot.synapse.adapter.models.UserInfoDigest;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.MultiMap;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.http.HttpMethod;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.client.HttpRequest;
 import io.vertx.ext.web.client.WebClient;
+
+import java.lang.management.BufferPoolMXBean;
 
 public class MxisdHandler extends AbstractVerticle {
     private final JsonHelper helper;
@@ -17,11 +22,19 @@ public class MxisdHandler extends AbstractVerticle {
 
     private WebClient webClient;
     private MainConfiguration config;
+    private final String loginUri;
+    private final String searchUri;
+    private final String searchBySinglePIDUri;
+    private final String bulkPIDSearchUri;
 
     public MxisdHandler(WebClient webClient, MainConfiguration config, JsonHelper helper) {
         this.helper = helper;
         this.webClient = webClient;
         this.config = config;
+        this.loginUri = "/auth/realms/testing/protocol/openid-connect/token";
+        this.searchUri = "";
+        this.searchBySinglePIDUri = "";
+        this.bulkPIDSearchUri = "";
     }
 
     public void loginHandler(RoutingContext routingContext) {
@@ -32,14 +45,13 @@ public class MxisdHandler extends AbstractVerticle {
 
         MultiMap form = helper.getUserForm(keycloakPassword, username);
 
-        webClient.post(443, config.KEYCLOAK_HOST, config.KEYCLOAK_CLIENT_URI)
-                .putHeader("Authorization", config.KEYCLOAK_CLIENT_BASIC)
-                .putHeader("content-type", "application/x-www-form-urlencoded")
-                .ssl(true)
+        HttpRequest<Buffer> request = generateAccessTokenRequest(loginUri);
+
+        request
                 .sendForm(form, ar -> {
-                    logger.info("form : " + form.toString());
                     if (ar.succeeded()) {
                         if (ar.result().statusCode() == 200) {
+                            logger.info("pouet");
                             JsonObject keycloakResponse = ar.result().bodyAsJsonObject();
                             UserInfoDigest userinfo = helper.extractTokentInfo(keycloakResponse
                                     .getString("access_token"));
@@ -49,6 +61,8 @@ public class MxisdHandler extends AbstractVerticle {
                                             .encodePrettily());
 
                         } else if (ar.result().statusCode() == 401) {
+                            logger.info("pouetpouet");
+
                             UserInfoDigest userInfoDigest = new UserInfoDigest(
                                     "", form.get("username"), false);
                             routingContext.response().setStatusCode(401);
@@ -80,5 +94,14 @@ public class MxisdHandler extends AbstractVerticle {
 
     public static void healthCheckHandler(RoutingContext routingContext) {
 
+    }
+
+    private HttpRequest<Buffer> generateAccessTokenRequest(String uri) {
+        HttpRequest<Buffer> request = this.webClient.post(443, config.KEYCLOAK_HOST, uri);
+        request.headers().add("Authorization", config.KEYCLOAK_CLIENT_BASIC);
+        request.headers().add("content-type", "application/x-www-form-urlencoded");
+        request.ssl(true);
+        request.method(HttpMethod.POST);
+        return request;
     }
 }
